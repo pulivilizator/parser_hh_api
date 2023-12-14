e9829edce4b44c7917f2d04b590f92fac5496c30
Dmitriy <112427972+pulivilizator@users.noreply.github.com>
2023-12-14 14:38:23 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,6 +2,5 @@
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
 source "$SCRIPT_DIR"/venv/bin/activate
 cd "$SCRIPT_DIR"
-pwd
 python3 "$SCRIPT_DIR"/main.py
 deactivate
